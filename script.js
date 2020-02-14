19284de685c1f0c51e305fe098ad1035fb336853
DENÍLSON MARCIO DOS SANTOS <48038716+DenilsonMarcio@users.noreply.github.com>
2020-02-13 23:56:43 -0300

    Introdução Jquery

---
+++ script.js
@@ -0,0 +1,16 @@
+var nome = "Denilson";
+var anonasc = 1982;
+var anoatual = 2020;
+var idade = anoatual - anonasc;
+
+if(idade<=35){
+    alert(nome);
+    alert(idade);
+    alert("Esta na flor da idade ainda");
+}else{
+    alert(nome);
+    alert(idade);
+    alert("Tá velhinho hein !");
+}
+console.log("Teste com console.log");
+
